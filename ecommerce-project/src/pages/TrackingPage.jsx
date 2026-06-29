f701893611e5ccd369bc09c81a6ff66bccff95a8
manasseh ibrahim <imanasseh03@gmail.com>
2026-06-29 14:21:02 +0100

    split header into a component and use link and move tracking to jsx

---
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -0,0 +1,85 @@
+import './TrackingPage.css'
+import './header.css'
+import logo from '../assets/images/logo-white.png';
+import mobileLogo from '../assets/images/mobile-logo-white.png';
+import searchIcon from '../assets/images/icons/search-icon.png';
+import cartIcon from '../assets/images/icons/cart-icon.png';
+import socksImage from '../assets/images/products/athletic-cotton-socks-6-pairs.jpg';
+
+
+export function TrackingPage() {
+    return (
+        <>
+            <title>Tracking</title>
+            <div className="header">
+                <div className="left-section">
+                    <a href="/" className="header-link">
+                        <img className="logo"
+                            src={logo} />
+                        <img className="mobile-logo"
+                            src={mobileLogo} />
+                    </a>
+                </div>
+
+                <div className="middle-section">
+                    <input className="search-bar" type="text" placeholder="Search" />
+
+                    <button className="search-button">
+                        <img className="search-icon" src={searchIcon} />
+                    </button>
+                </div>
+
+                <div className="right-section">
+                    <a className="orders-link header-link" href="/orders">
+
+                        <span className="orders-text">Orders</span>
+                    </a>
+
+                    <a className="cart-link header-link" href="/checkout">
+                        <img className="cart-icon" src={cartIcon} />
+                        <div className="cart-quantity">3</div>
+                        <div className="cart-text">Cart</div>
+                    </a>
+                </div>
+            </div>
+
+            <div className="tracking-page">
+                <div className="order-tracking">
+                    <a className="back-to-orders-link link-primary" href="/orders">
+                        View all orders
+                    </a>
+
+                    <div className="delivery-date">
+                        Arriving on Monday, June 13
+                    </div>
+
+                    <div className="product-info">
+                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                    </div>
+
+                    <div className="product-info">
+                        Quantity: 1
+                    </div>
+
+                    <img className="product-image" src={socksImage} />
+
+                    <div className="progress-labels-container">
+                        <div className="progress-label">
+                            Preparing
+                        </div>
+                        <div className="progress-label current-status">
+                            Shipped
+                        </div>
+                        <div className="progress-label">
+                            Delivered
+                        </div>
+                    </div>
+
+                    <div className="progress-bar-container">
+                        <div className="progress-bar"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
